b5c997805c33f4f5383824f05fafc957afad16bd
Luong Minh <ngocminhluong182@gmail.com>
2024-01-03 13:04:14 +0700

    fix docusaurus build url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://8xFF.github.io/atm0s-docs",
+  url: "https://8xFF.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
